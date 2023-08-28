2d21666b0a1bdf689d031ba62534bfc46656b2ad
Carlos <carlosemaya@gmail.com>
2023-08-28 17:59:02 -0300

    aula 02 pt 02

--- src/main/java/br/com/agenda/dao/ContatoDAO.java
+++ src/main/java/br/com/agenda/dao/ContatoDAO.java
@@ -1,11 +1,56 @@
 package br.com.agenda.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Date;
+
+import br.com.agenda.factory.ConnectionFactory;
+import br.com.agenda.model.Contato;
+
 public class ContatoDAO {
+	
+	//Atenção no import sql.Date, pode ser util.Date
 	
 	//CRUD
 	
-	public void save() {
+	public void save(Contato contato) {
 		
+		String sql = "INSERT INTO contatos(nome, idade, datacadastro) VALUES (?, ?, ?)";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			//Criar uma conexão com o banco de dados
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			//Aula 02 11:19, MUITA ATENÇÃO AQUI
+			//PreparedStatement para executar uma query
+			pstm = conn.prepareStatement(sql);
+			//Adicionar os valores esperados pela query
+			pstm.setString(1, contato.getNome());
+			pstm.setInt(2, contato.getIdade());
+			pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+			
+			//Executar query
+			pstm.execute();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+			//Fechar conexões
+			try {
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
